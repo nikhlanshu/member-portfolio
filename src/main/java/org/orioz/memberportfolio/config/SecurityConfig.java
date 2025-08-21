0a93c91198c2add240f4d9acdb7c12024c44f7b5
nikhlanshu <nikhlanshu@gmail.com>
2025-08-21 15:55:04 +1000

    adding entitlement

--- src/main/java/org/orioz/memberportfolio/config/SecurityConfig.java
+++ src/main/java/org/orioz/memberportfolio/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
